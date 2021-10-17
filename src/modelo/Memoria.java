d427428b9145f54120eb0969555e0a77f694e2fa
Mayke <maykekend@gmail.com>
2021-10-17 13:54:33 -0300

    Display com textoAtual

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -2,4 +2,23 @@ package modelo;
 
 public class Memoria {
 
+	// Criando uma instância
+	private static Memoria instancia = new Memoria();
+	
+	private String textoAtual = ""; 
+	
+	// Construtor em que controla a criacao dentro da propria classe
+	private Memoria() {
+	}
+	
+	// Método get, consigo pegar uma única instancia que foi criada na classe Memoria
+	public static Memoria getInstancia() {
+		return instancia;
+	}
+
+	// Método get para pegar o textoAtual no Display
+	public String getTextoAtual() {
+		return textoAtual.isEmpty() ? "0" : textoAtual; // Se for verdadeiro retorna "0", se não, retorna o textoAtual = "" no display
+	}
+	
 }
--- src/visao/Display.java
+++ src/visao/Display.java
@@ -3,6 +3,7 @@ package visao;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import modelo.Memoria;
 
 import java.awt.Color;
 import java.awt.Font;
@@ -16,7 +17,8 @@ public class Display extends JPanel{
 	public Display() {
 		//setBackground(Color.GREEN);
 		setBackground(new Color(31,31,31));
-		label = new JLabel("123");
+		//label = new JLabel("123"); 
+		label = new JLabel(Memoria.getInstancia().getTextoAtual()); // vamos substituir por texto atual
 		label.setForeground(Color.WHITE);
 		label.setFont(new Font("Segoe UI", Font.PLAIN, 45));
 		
